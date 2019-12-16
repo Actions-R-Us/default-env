7946e9c02c7bc4ad2529848432faf32abe600c9d
smac89 <nobleman.code@gmx.com>
2019-12-16 14:51:24 -0600

    Replace python task with typescript

---
+++ action.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core';
+
+const env_vars = [
+    'HOME',
+    'GITHUB_WORKFLOW',
+    'GITHUB_ACTION',
+    'GITHUB_ACTIONS',
+    'GITHUB_ACTOR',
+    'GITHUB_REPOSITORY',
+    'GITHUB_EVENT_NAME',
+    'GITHUB_EVENT_PATH',
+    'GITHUB_WORKSPACE',
+    'GITHUB_SHA',
+    'GITHUB_REF',
+    'GITHUB_HEAD_REF',
+    'GITHUB_BASE_REF'
+];
+
+async function run(): Promise<void> {
+    try {
+        for (const env_var of env_vars) {
+            if (env_var in process.env) {
+                core.debug(`Export: ${env_var}=${process.env[env_var]}`);
+                core.exportVariable(env_var, process.env[env_var])
+            }
+        }
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+run();
